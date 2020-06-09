c6d7cc70b065760f858f55a0035f76027554011a
AnhDT1 <anhdt1@daivietgroup.net.vn>
2020-06-09 11:08:58 +0700

    deploy

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {useState, useEffect} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import {robots} from '../robots.js';
@@ -6,54 +6,40 @@ import Scroll from '../components/Scroll';
 import './App.css';
 var httpClient = require('../HttpClient');
 
-class App extends Component {
-	constructor(){
-		super();
-		this.state = {
-			robots: [],
-			searchField: ''
-		};
-	}
+const App = () => {
+	const [robots, setRobots] = useState([]);
+	const [searchField, setSearchField] = useState('');
 
-	onSearchFieldChange = (searchField) => {
-		this.setState({
-			searchField: searchField,
-			robots: robots.filter(function(item){
+	const onSearchFieldChange = (searchField) => {
+		setSearchField(searchField);
+		setRobots(robots.filter(function(item){
 				return item.name.toLowerCase().includes(searchField.toLowerCase())
-			})
-		});
+			}));
 	}
 
-	updateData = (data) => {
-		this.setState({
-			robots: data
-		});
+	const updateData = (data) => {
+		setRobots(data);
 	}
 
-	componentDidMount(){
+	useEffect(()=> {
 		httpClient.makeGetRequest({
 			host: 'jsonplaceholder.typicode.com/',
 			path: 'users',
 			method: 'GET'
-		}, this.updateData);
-	}
+		}, updateData);
+	});
 
-	render(){
-		if(this.state.robots.length <=0){
-			return <h1>Loading</h1>;
-		}
-
-		return (
+	return robots.length <=0 ?
+			<h1>Loading</h1>:
+			(
 			<div className='tc'>
 				<h1 className="f2">RoboFriends</h1>
-				<SearchBox onSearchFieldChange= {this.onSearchFieldChange}/>
+				<SearchBox onSearchFieldChange= {onSearchFieldChange}/>
 				<Scroll>
-					<CardList robots={this.state.robots}/>
+					<CardList robots={robots}/>
 				</Scroll>
 			</div>
 		);
-	}
-	
 };
 
 export default App;
